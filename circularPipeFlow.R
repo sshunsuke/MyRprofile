52cff87e0e7d4c8966db42faa18785595470205f
shunsuke <s_shunske@yahoo.co.jp>
2019-10-22 17:38:30 +0800

    Modified MukherjeeBrill$selectCoefficients() to support vector.

--- circularPipeFlow.R
+++ circularPipeFlow.R
@@ -7,17 +7,15 @@ source("defaultSetting.R")
 
 # =============================================================================
 
-
 MukherjeeBrill <- list()
 
 MukherjeeBrill$coefficients <- cbind(
-	c(-0.380113, 0.129875, -0.119788,  2.343227, 0.475686, 0.288657),
-	c(-1.330282, 4.808139,  4.171584, 56.262268, 0.079951, 0.504887),
-	c(-0.516644, 0.789805,  0.551627, 15.519214, 0.371771, 0.393952)
+	c(-0.380113, 0.129875, -0.119788,  2.343227, 0.475686, 0.288657),   # Up
+	c(-1.330282, 4.808139,  4.171584, 56.262268, 0.079951, 0.504887),   # DownStratified
+	c(-0.516644, 0.789805,  0.551627, 15.519214, 0.371771, 0.393952)    # DownOther
 )
 colnames(MukherjeeBrill$coefficients) <- c("Up", "DownStratified", "DownOther")
 rownames(MukherjeeBrill$coefficients) <- paste("C", 1:6, sep="")
-
 
 
 # -----------------------------------------------------------------------------
@@ -74,9 +72,6 @@ MukherjeeBrill$calculateDLNs = function(vsG, vsL, D, densityG, densityL, viscosi
 
 
 
-
-
-
 # Check flow regime.
 # There are four flow regime types are defined in this function. 
 #   1: Stratified
@@ -90,37 +85,37 @@ MukherjeeBrill$checkFlowRegime <- function(DLNs) {
     flowRegime <- 2  # annular 
     
     if (NGv > NGvSM) {
-      return (flowRegime)
+      return (flowRegime)  # Annular
     }
     
     if (angle > 0) {
       # Upfill
       if (NLv > NLvBS_up) {
-        flowRegime <- flowRegime <- 4  # bubbly
+        flowRegime <- 4  # bubbly
       } else {
-        flowRegime <- flowRegime <- 3  # slug
+        flowRegime <- 3  # slug
       }
     } else if (abs(angle) > deg2rad(-30)) {
       # Downhill
       if (NGv > NGvBS) {
         if (NLv > NLvST) {
-          flowRegime <- flowRegime <- 3  # Slug
+          flowRegime <- 3  # Slug
         } else {
-          flowRegime <- flowRegime <- 1  # Stratified
+          flowRegime <- 1  # Stratified
         }
       } else {
-        flowRegime <- flowRegime <- 4    # bubbly
+        flowRegime <- 4    # bubbly
       }
     } else {
       # DownStratified
       if (NLv > NLvST) {
         if (NGv > NGvBS) {
-          flowRegime <- flowRegime <- 3    # Slug
+          flowRegime <- 3    # Slug
         } else {
-          flowRegime <- flowRegime <- 4    # bubbly
+          flowRegime <- 4    # bubbly
         }
       } else {
-        flowRegime <- flowRegime <- 1  # Stratified
+        flowRegime <- 1  # Stratified
       }
     }
     
@@ -132,22 +127,20 @@ MukherjeeBrill$checkFlowRegime <- function(DLNs) {
 
 
 MukherjeeBrill$selectCoefficients <- function(DLNs, flowRegime) {
-	if (DLNs$angle <= 0) {
-		if (flowRegime == 1) {
-			return(MukherjeeBrill$coefficients[,"DownStratified"])
-		} else {
-			return(MukherjeeBrill$coefficients[,"DownOther"])
-		}
-	}
-	MukherjeeBrill$coefficients[,"Up"]
+	index <- ifelse(DLNs$angle > 0,
+									1,
+									ifelse((flowRegime == 1), 2, 3))
+	MukherjeeBrill$coefficients[,index]
 }
+
+
 	
 
 MukherjeeBrill$holdup <- function(DLNs, flowRegime) {
 	co <- MukherjeeBrill$selectCoefficients(DLNs, flowRegime)
 	
-	t1 <- co[1] + (co[2] * sin(DLNs$angle)) + (co[3] * sin(DLNs$angle)^2) + (co[4] * DLNs$NL^2)
-	t2 <- DLNs$NGv^co[5] / DLNs$NLv^co[6]
+	t1 <- co[1,] + (co[2,] * sin(DLNs$angle)) + (co[3,] * sin(DLNs$angle)^2) + (co[4,] * DLNs$NL^2)
+	t2 <- DLNs$NGv^co[5,] / DLNs$NLv^co[6,]
 	exp(t1 * t2)
 }
 
@@ -161,9 +154,7 @@ MukherjeeBrill$dPdL <- function(DLNs, flowRegime) {
 	densityMixN <- densityG * (1 - HLnoslip) + densityL * HLnoslip        # (3.23)
 	viscosityMixS <- viscosityG * (1 - HL) + viscosityL * HL              # (3.19)
 	viscosityMixN <- viscosityG * (1 - HLnoslip) + viscosityL * HLnoslip  # (3.21)
-	
 
-	
 	if (flowRegime == 3 | flowRegime == 4) {
 		# Slug or Bubble
 		Re <- densityMixN * vmix * DLNs$D / viscosityMixN
@@ -179,6 +170,8 @@ MukherjeeBrill$dPdL <- function(DLNs, flowRegime) {
 		
 		
 	} else if (flowRegime == 1) {
+		# Stratified
+		
 		
 	} else {
 		# error
@@ -212,9 +205,10 @@ fr <- MukherjeeBrill$checkFlowRegime(exam)
 MukherjeeBrill$holdup(exam, fr)
 
 
-exam2 <- MukherjeeBrill$calculateDLNs(ft2m(c(3.86,0.1)), ft2m(3.97), inch2m(6), 5.88*ratio, 47.61*ratio, UC$cP2Pas(0.016), UC$cP2Pas(0.97), UC$dynpcm2Npm(8.41), pi/2)
-
-
+exam2 <- MukherjeeBrill$calculateDLNs(ft2m(c(3.86, 0.1)), ft2m(3.97), inch2m(6), 5.88*ratio, 47.61*ratio, UC$cP2Pas(0.016), UC$cP2Pas(0.97), UC$dynpcm2Npm(8.41), c(pi/2, -0.1))
+fr2 <- MukherjeeBrill$checkFlowRegime(exam2)
+MukherjeeBrill$selectCoefficients(exam2, fr2)
+MukherjeeBrill$holdup(exam2, fr2)
 
 
 
@@ -256,6 +250,17 @@ if (FALSE) {
 	
 	NL
 	
+	
+	{
+		if (DLNs$angle <= 0) {
+			if (flowRegime == 1) {
+				return(MukherjeeBrill$coefficients[,"DownStratified"])
+			} else {
+				return(MukherjeeBrill$coefficients[,"DownOther"])
+			}
+		}
+		MukherjeeBrill$coefficients[,"Up"]
+	}
 }
 
 
